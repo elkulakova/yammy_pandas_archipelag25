b1ded3125a2981cafe255f98a3aa3a5ac86c04cc
LiudmilaSafonova <lyusia200603@gmail.com>
2025-08-01 23:25:03 +0300

    обучение модели yolo.pt

--- test_human_model.py
+++ test_human_model.py
@@ -2,11 +2,15 @@ from ultralytics import YOLO
 import os
 import torch
 import cv2
-import albumentations as A
 from ultralytics.data.dataset import YOLODataset
 from ultralytics.data.augment import Albumentations
 from torch.utils.data import DataLoader
 import requests
+from key import TOKEN, CHAT_ID
+from salt_pepper_custom import SaltAndPepperNoise #импорт класса
+import albumentations as A
+
+A.SaltAndPepperNoise = SaltAndPepperNoise
 
 
 torch.backends.cudnn.benchmark = True  # Ускорит вычисления на 10-15%
@@ -23,8 +27,45 @@ if __name__ == "__main__":
 train: images/train
 val: images/val
 test: images/test
+
 names:
   0: person
+
+augment: True
+
+albumentations:
+  train:
+    - name: RandomBrightnessContrast
+      brightness_limit: 0.2
+      contrast_limit: 0.2
+      p: 0.5
+    - name: Blur
+      blur_limit: 3
+      p: 0.1
+    - name: ShiftScaleRotate
+      shift_limit: 0.05
+      scale_limit: 0.1
+      rotate_limit: 15
+      p: 0.5
+    - name: HueSaturationValue
+      hue_shift_limit: 20
+      sat_shift_limit: 30
+      val_shift_limit: 20
+      p: 0.5
+    - name: Fog
+      p: 0.2
+    - name: ISONoise
+      intensity: (0.1, 0.3)
+      p: 0.3
+    - name: JpegCompression
+      quality_lower: 70
+      p: 0.2
+    - name: RandomShadow
+      p: 0.2
+    - name: RandomSnow
+      p: 0.1
+    - name: SaltAndPepperNoise
+      p: 0.1
 """
 
     # Создаем YAML
@@ -35,7 +76,7 @@ names:
 
 
 
-    model = YOLO("yolov8n.pt")
+    model = YOLO("yolov8s.pt")
 
     # Обучение
     train_results = model.train(
@@ -54,18 +95,10 @@ names:
         pretrained=True,
         verbose=True,
         lr0=1e-4,
-
-
-        augment=True,
-        degrees=10,            # Поворот ±10°
-        scale=0.5,             # Масштабирование (0.5–1.5)
-        shear=2,               # Сдвиг по оси до 2°
-        hsv_h=0.005,           # Сдвиг оттенка
-        hsv_s=0.2,             # Сдвиг насыщенности
-        hsv_v=0.2, 
-        mosaic=1,
-        mixup=0.2            # Сдвиг яркости
         
+        augment=True,
+        mosaic=1,
+        mixup=0.2
     )
 
     print(train_results.speed)
@@ -77,9 +110,13 @@ names:
     results[0].show()
 
     # path = model.export(format="onnx")
+    # это код для телеграм бота, который отправляет результаты 
+    # обучения модели в виде графиков, лучшую модель, 
+    # результаты обучения по поколения в формате csv
+    # но настроен пока не до конца, надо менять циферки под каждое обучение
 
-    TOKEN = "8336957289:AAH6hShu1bDZwUd5KfMTPIs8FMJhyI2EwY4"
-    CHAT_ID = "5179925114"
+    TOKEN = TOKEN
+    CHAT_ID = CHAT_ID
 
 
     def send_message(text):
@@ -93,24 +130,24 @@ names:
             requests.post(url, data={"chat_id": CHAT_ID}, files={"document": f})
 
 
-    # пример использования в конце кода
-    send_message("Код завершил работу! 66")
-    send_file("runs\\detect\\train66\\args.yaml")
-    send_file("runs\\detect\\train66\\weights\\best.pt")
-    send_file('runs\\detect\\train66\\BoxF1_curve.png')
-    send_file('runs\\detect\\train66\\BoxP_curve.png')
-    send_file('runs\\detect\\train66\\BoxPR_curve.png')
-    send_file('runs\\detect\\train66\\BoxR_curve.png')
-    send_file('runs\\detect\\train66\\confusion_matrix.png')
-    send_file('runs\\detect\\train66\\confusion_matrix_normalized.png')
-    send_file('runs\\detect\\train66\\labels.jpg')
-    send_file('runs\\detect\\train66\\results.csv')
-    send_file('runs\\detect\\train66\\results.png')
-    send_file('runs\\detect\\train66\\train_batch0.jpg')
-    send_file('runs\\detect\\train66\\train_batch1.jpg')
-    send_file('runs\\detect\\train66\\train_batch2.jpg')
-    send_file('runs\\detect\\train66\\val_batch0_labels.jpg')
-    send_file('runs/detect/train66/val_batch1_labels.jpg')
-    send_file('runs/detect/train66/val_batch1_pred.jpg')
-    send_file('runs/detect/train66/val_batch2_labels.jpg')
-    send_file('runs/detect/train66/val_batch2_pred.jpg')
+    # менять циферки тутъ :)
+    send_message("Код завершил работу! 71")
+    send_file("runs\\detect\\train71\\args.yaml")
+    send_file("runs\\detect\\train71\\weights\\best.pt")
+    send_file('runs\\detect\\train71\\BoxF1_curve.png')
+    send_file('runs\\detect\\train71\\BoxP_curve.png')
+    send_file('runs\\detect\\train71\\BoxPR_curve.png')
+    send_file('runs\\detect\\train71\\BoxR_curve.png')
+    send_file('runs\\detect\\train71\\confusion_matrix.png')
+    send_file('runs\\detect\\train71\\confusion_matrix_normalized.png')
+    send_file('runs\\detect\\train71\\labels.jpg')
+    send_file('runs\\detect\\train71\\results.csv')
+    send_file('runs\\detect\\train71\\results.png')
+    send_file('runs\\detect\\train71\\train_batch0.jpg')
+    send_file('runs\\detect\\train71\\train_batch1.jpg')
+    send_file('runs\\detect\\train71\\train_batch2.jpg')
+    send_file('runs\\detect\\train71\\val_batch0_labels.jpg')
+    send_file('runs/detect/train71/val_batch1_labels.jpg')
+    send_file('runs/detect/train71/val_batch1_pred.jpg')
+    send_file('runs/detect/train71/val_batch2_labels.jpg')
+    send_file('runs/detect/train71/val_batch2_pred.jpg')
